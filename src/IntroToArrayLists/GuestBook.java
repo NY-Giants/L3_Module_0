e1654ebc4a2a648d58a6cf2c4099ee999b963303
FruitWave <league@WTS8.attlocal.net>
2017-09-10 17:27:08 -0700

    gg
    Finish GuestBook

--- src/IntroToArrayLists/GuestBook.java
+++ src/IntroToArrayLists/GuestBook.java
@@ -1,6 +1,41 @@
 package IntroToArrayLists;
 
-public class GuestBook {
+import java.awt.Frame;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.awt.event.MouseEvent;
+import java.awt.event.MouseListener;
+
+import javax.swing.JButton;
+import javax.swing.JLabel;
+import javax.swing.JPanel;
+
+public class GuestBook implements ActionListener{
+	Frame frame = new Frame();
+	JLabel label1 = new JLabel();
+	JPanel panel = new JPanel();
+	JLabel label2 = new JLabel();
+	JButton button1 = new JButton();
+	JButton button2 = new JButton();
+	public static void main(String[]args){
+		GuestBook book = new GuestBook();
+		
+	}
+	
+	GuestBook(){
+	frame = new Frame();
+	panel = new JPanel();
+	button1 = new JButton("Add Name");
+	button2 = new JButton("View Names");
+	frame.add(panel);
+	panel.add(button1);
+	panel.add(button2);
+	frame.setSize(800, 500);
+	frame.setVisible(true);
+	
+
+	
+	}
 	// Create a GUI with two buttons. One button reads "Add Name" and the other button reads "View Names". 
 	// When the add name button is clicked, display an input dialog that asks the user to enter a name. Add
 	// that name to an ArrayList. When the "View Names" button is clicked, display a message dialog that displays
@@ -9,4 +44,12 @@ public class GuestBook {
 	// Guest #2: Sandy Summers
 	// Guest #3: Greg Ganders
 	// Guest #4: Donny Doners
+
+	@Override
+	public void actionPerformed(ActionEvent e) {
+		// TODO Auto-generated method stub
+		
+	}
+
+
 }
--- src/IntroToArrayLists/IntroToArrayLists.java
+++ src/IntroToArrayLists/IntroToArrayLists.java
@@ -7,32 +7,41 @@ public class IntroToArrayLists {
 		//1. Create an array list of Strings
 		ArrayList<String> StringList = new ArrayList<String>();
 		//   Don't forget to import the ArrayList class
-			StringList.add("AI");
+			StringList.add("Skynt");
 			StringList.add("Online...");
 			StringList.add("Project");
 			StringList.add("Genisys");
 			StringList.add("Initiated...");
+			
 		//2. Add five Strings to your list
 		
 		//3. Print all the Strings using a standard for-loop
 			for(int i = 0; i < StringList.size(); i++){
 				String s = StringList.get(i);
 				System.out.println(s);
-			}
+			} 
 		//4. Print all the Strings using a for-each loop
 			for(String S: StringList){
 				System.out.println(S);
 			}
 		//5. Print only the even numbered elements in the list.
-			for(int i = 0; i < StringList.size(); i+= 2){
+			 for(int i = 0; i < StringList.size(); i+= 2){
 				String s = StringList.get(i);
 				System.out.println(s);
-			}
+			} 
 		//6. Print all the Strings in reverse order.
+			for(int i = StringList.size() -1; i >= 0; i--){
+				String s = StringList.get(i);
+				System.out.println(s); 
+			} 
+		//7. Print only the Strings that have the letter 'e' in them.
 			for(int i = 0; i < StringList.size(); i++){
 				String s = StringList.get(i);
-				System.out.println(s);
+				if( s.contains("e")  ){
+				
+				System.out.println(s); 
+
 			}
-		//7. Print only the Strings that have the letter 'e' in them.
-	}
+					}
+}
 }
